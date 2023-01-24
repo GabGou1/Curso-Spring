8678a77260f10d36f83406193d2d2abc996688eb
GabGou1 <gabrielriberikoa@gmail.com>
2023-01-24 13:45:30 -0300

    Created unit test for find by id test

---
+++ src/test/java/com/cursospring/cursospring/service/BookServiceTest.java
@@ -0,0 +1,37 @@
+package com.cursospring.cursospring.service;
+
+import com.cursospring.cursospring.dto.BookDTO;
+import com.cursospring.cursospring.entity.Book;
+import com.cursospring.cursospring.repository.BookRepository;
+import com.cursospring.cursospring.utils.BookUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static com.cursospring.cursospring.utils.BookUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+    @Mock
+    private BookRepository bookRepository;
+    @InjectMocks
+    private BookService bookService;
+
+    @Test
+    void WhenGivenExistingIdThenReturnThisBook() {
+        Book expectedFoundBook = createFakeBook();
+        when(bookRepository.findById(expectedFoundBook.getId())).thenReturn(Optional.of(expectedFoundBook));
+        BookDTO bookDTO = bookService.findById(expectedFoundBook.getId());
+        assertEquals(expectedFoundBook.getName(), bookDTO.getName());
+        assertEquals(expectedFoundBook.getIsbn(), bookDTO.getIsbn());
+        assertEquals(expectedFoundBook.getPublisherName(), bookDTO.getPublisherName());
+    }
+}
